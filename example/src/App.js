dd8896b33df0b205f5c0498876ec37cf18dcca31
Leonardo Giovanni Scur <kroltan1@gmail.com>
2018-08-17 18:39:10 -0300

    temporary stash

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import ExampleComponent from 'react-motion-blur'
+import { MotionBlur } from 'react-motion-blur';
+
 
 export default class App extends Component {
+  state = {x: 0, y: 0};
+
   render () {
+    const {x, y} = this.state;
+
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
+      <div
+        style={{width: "100%", height: "100%"}}
+        onMouseMove={ev => this.setState({
+          x: ev.clientX,
+          y: ev.clientY,
+        })}
+      >
+        <MotionBlur
+          active={true}
+          radius={10}
+          style={{
+            position: "absolute",
+            top: y,
+            left: x,
+          }}
+        >
+          Hello there!
+        </MotionBlur>
       </div>
     )
   }
