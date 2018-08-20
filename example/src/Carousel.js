6d653aa662e9fae50a454dcfccb9884cf3994d51
Leonardo Giovanni Scur <kroltan1@gmail.com>
2018-08-19 21:58:41 -0300

    blah

---
+++ example/src/Carousel.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {MotionBlur} from 'react-motion-blur';
+
+
+export function Carousel({images}) {
+  return (
+    <div className="Carousel box">
+      <MotionBlur intensity={2}>
+        <div className="Carousel-layout">
+          {images.map(image => (
+            <img
+              key={image}
+              width={315}
+              height={250}
+              src={image}
+              alt="Carousel item"
+            />
+          ))}
+        </div>
+      </MotionBlur>
+    </div>
+  );
+}
